c9e7f470b0320d292717bb81f0e25b1b6ece7349
kenoldkhalika <48180424+kenoldkhalika@users.noreply.github.com>
2019-04-26 15:54:19 +0200

    initial commit

---
+++ question.js
@@ -0,0 +1,82 @@
+var questions=[
+{
+    "question": "The first mechanical computer designed by Charles Babbage was called?",
+    "option1": "Abacus",
+    "option2": "Analytical engine",
+    "option3": "Calculator",
+    "option4": "Processor",
+    "answer": "2"
+},
+
+{
+    "question": "Which of the following is most poweful type of computer",
+    "option1": "Super-micro",
+    "option2": "Super conductor",
+    "option3": "Super computer",
+    "option4": "Megaframe",
+    "answer": "3"
+},
+{
+    "question": "C  is?",
+    "option1": "A third generation high level language",
+    "option2": "A machine language",
+    "option3": "An assembly  language",
+    "option4": "All of the above",
+    "answer": "1"
+},
+{
+    "question": "Web page is written using?",
+    "option1": "FTP",
+    "option2": "HTTP",
+    "option3": "HTML",
+    "option4": "URL",
+    "answer": "3"
+},
+{
+    "question": "UNIVAC is example of",
+    "option1": "First generation computer",
+    "option2": "Second generation computer",
+    "option3": "Third generation computer",
+    "option4": "Fourth generation computer",
+    "answer": "1"
+},
+{
+    "question": "Which of the following is an example of non volatile?",
+    "option1": "VLSI",
+    "option2": "LSI",
+    "option3": "ROM",
+    "option4": "RAM",
+    "answer": "3"
+},
+{
+    "question": "Graphic interfaces were first used in xerox product is called?",
+    "option1": "Ethernet",
+    "option2": "Inter LISP",
+    "option3": "Small talk",
+    "option4": "Zeta LISP",
+    "answer": "1"
+},
+{
+    "question": "Find the odd one out 7",
+    "option1": "ORACLE",
+    "option2": "SYBASE",
+    "option3": "C",
+    "option4": "INFORMIX",
+    "answer": "3"
+},
+{
+    "question": "Which is a single integrated circuit",
+    "option1": "GATE",
+    "option2": "Mother Board",
+    "option3": "Chip",
+    "option4": "CPU",
+    "answer": "1"
+},
+{  "question": "What is the correct syntax for referring to an external script  called 'xxx.js'?",
+    "option1": "script href='xxx.js'",
+    "option2":"script name='xxx.js'",
+    "option3":"script src='xxx.js'",
+    "option4":"script file='xxx.js'",
+"answer": "3"
+},
+];
